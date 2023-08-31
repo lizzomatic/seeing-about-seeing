e4618ba876f43ef85bae8febd2b64d85c18a247e
lizzomatic <lizzomatic@gmail.com>
2023-08-31 09:14:39 -0700

    removed eslint

--- src/app/drawing/page.tsx
+++ src/app/drawing/page.tsx
@@ -5,25 +5,25 @@ import React from 'react';
 import { Header } from "../components/header/header";
 import { Footer } from "../components/footer/footer";
 import { Gallery } from "react-grid-gallery";
-//import Lightbox from "react-image-lightbox";
-import "react-image-lightbox/style.css";
 import { images, CustomImage } from "./images";
+//import Lightbox from "react-image-lightbox";
+// import "react-image-lightbox/style.css";
 
 
 
 export default () => {
-  const [index, setIndex] = useState(-1);
+  // const [index, setIndex] = useState(-1);
 
-  const currentImage = images[index];
-  const nextIndex = (index + 1) % images.length;
-  const nextImage = images[nextIndex] || currentImage;
-  const prevIndex = (index + images.length - 1) % images.length;
-  const prevImage = images[prevIndex] || currentImage;
+  // const currentImage = images[index];
+  // const nextIndex = (index + 1) % images.length;
+  // const nextImage = images[nextIndex] || currentImage;
+  // const prevIndex = (index + images.length - 1) % images.length;
+  // const prevImage = images[prevIndex] || currentImage;
 
-  const handleClick = (index: number, item: CustomImage) => setIndex(index);
-  const handleClose = () => setIndex(-1);
-  const handleMovePrev = () => setIndex(prevIndex);
-  const handleMoveNext = () => setIndex(nextIndex);
+  // const handleClick = (index: number, item: CustomImage) => setIndex(index);
+  // const handleClose = () => setIndex(-1);
+  // const handleMovePrev = () => setIndex(prevIndex);
+  // const handleMoveNext = () => setIndex(nextIndex);
 
   return (
     <div className='bg-white px-4'>
